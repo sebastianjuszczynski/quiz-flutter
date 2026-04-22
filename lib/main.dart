3dbe47e25107ee6ea90d70edf34166972f4fae98
Sebastian Juszczyński <sebastianjuszczynski@yahoo.com>
2026-04-22 10:04:49 +0200

    Refactor app structure: replace start screen with quiz entry point and add questions screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'start_screen.dart';
+import 'quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
